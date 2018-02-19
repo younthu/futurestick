d36c043ef51f7e6186ceb1aa4cd5448170789d79
Andrew Yang <evil_eve_live@outlook.com>
2018-02-20 00:53:35 +0800

    show AG0 successful

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # futurestick
 read futures tick from sina api
+
+# Run sample command
+
+~~~
+python app.py --count=3
+~~~
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import click
+import requests
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
@@ -9,5 +14,18 @@ def hello(count, name):
     for x in range(count):
         click.echo('Hello %s!' % name)
 
+@cli.command()
+@click.option('--quote', default='AG0', help='index of futures quote, could be a comma splitted array')
+def get_quote(quote):
+    """Getting data from sina api"""
+    response = requests.get("http://hq.sinajs.cn/list="+quote)
+#    print(response.text)
+    #print(response.json())
+    arr = response.text.split('=')[1].strip('"').split(',')
+    print(arr[0]+","+arr[16]+"," + arr[15] + "," + arr[17] + "," + arr[5] + "    " + arr[8]) # please check ticket #74 for detailed fields explain
+
+
 if __name__ == '__main__':
-    hello()
+   # hello()
+   cli()
+   pass
