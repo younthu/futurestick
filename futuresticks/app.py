f5c90150937b8d4196e1ec740e42150d9a741b84
Andrew Yang <evil_eve_live@outlook.com>
2018-03-12 11:28:18 +0800

    notification while hitting limits

--- futuresticks/app.py
+++ futuresticks/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import click
 import requests
 from time import sleep
@@ -111,6 +112,53 @@ def get_quote(quote, repeat, save):
 
 
 @cli.command()
+@click.option('--code', '-c', help='code of tureus product')
+@click.option('--limit', '-l', help='when to alert. a positive number means top limit, a minus number means bottom limit')
+def watch(code, limit):
+    response = requests.get("http://hq.sinajs.cn/list="+code)
+    notified = False
+
+    while True:
+        linenum = 1
+
+        for arrraw in response.text.splitlines():
+            arr = arrraw.split('=')[1].strip(';').strip('"').split(',')
+            print(arr[0]+","+arr[16]+"," + arr[15] + "," + arr[17] + "," + arr[5] + "    " + arr[8])
+
+        #    if save:
+        #        collection.insert({'code':quote, 'tick': arr[8]})
+
+            if float(limit) < 0 and float(arr[8]) + float(limit) < 0:
+                print("break bottom limit:" + limit * -1)
+
+                if not notified:
+                    notify(title = 'Bottom limit hit',
+                       subtitle = 'it is going down',
+                       message = 'please pay attention to ' + arr[0] + ', it has hit bottom limit' + limit * -1 + " with value:" + arr[8])
+                    notified = True
+            elif float(limit) > 0 and float(arr[8]) - float(limit) > 0 :
+                print('hit up limit:' + limit)
+
+                if not notified:
+                    notify(title='Up limite hit',
+                           subtitle = 'it is going up',
+                           message = 'please pay attention to ' + arr[0] + ', it has hit up limit ' + limit + ' with value ' + arr[8])
+                    notified = True
+
+            else:
+                print('watching...')
+                if notified:
+                    notified = False
+
+            linenum = linenum + 1
+
+        sleep(1)
+
+        # clear buffer#
+        #  "\033[F" move one line up, '\r' move cursor back to beginning of line
+        print("\033[F" * (linenum + 1) + '\r')
+
+@cli.command()
 @click.option('--code', '-c', help='code of futures product')
 @click.option('--num', '-n', help='num of futures')
 @click.option('--price', '-p', help='price of futures product')
@@ -129,3 +177,10 @@ def stop_order(code, stop, num):
 if __name__ == '__main__':
     cli()
     pass
+
+# The notifier function, https://stackoverflow.com/questions/17651017/python-post-osx-notification/17651702#17651702
+def notify(title, subtitle, message):
+    t = '-title {!r}'.format(title)
+    s = '-subtitle {!r}'.format(subtitle)
+    m = '-message {!r}'.format(message)
+    os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
